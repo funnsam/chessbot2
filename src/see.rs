d457759ae5947677ef276cd896bb6fc32cc7d6a7
funnsam <funnsam8@gmail.com>
2024-12-16 20:26:39 +0800

    h

--- src/see.rs
+++ src/see.rs
@@ -22,7 +22,11 @@ pub fn see(game: &Game, m: ChessMove) -> i16 {
         let mut from = BitBoard::from_square(m.get_source());
         let mut combined = *game.board().combined();
         let mut stm = game.board().side_to_move();
-        let mut attadef = game.board().pseudo_attacks_to(m.get_dest(), combined);
+        let mut attadef = game.board().pseudo_attacks_to(m.get_dest(), combined, combined);
+        let mut may_xray = game.board().pieces(chess::Piece::Pawn) |
+            game.board().pieces(chess::Piece::Bishop) |
+            game.board().pieces(chess::Piece::Rook) |
+            game.board().pieces(chess::Piece::Queen);
         let mut gain = [0; 32];
         gain[0] = PIECE_VALUE[target.to_index()];
 
@@ -35,9 +39,17 @@ pub fn see(game: &Game, m: ChessMove) -> i16 {
             combined ^= from;
             // game.visualize(combined);
 
-            // TODO: attadef |= xrays
-            // 7k/4r3/4q3/8/4Q3/3P1B2/8/K7 b - - 0 1
-            // the black rook was covered and opened up by black queen currently not considered
+            if (from & may_xray).0 != 0 {
+                // TODO: attadef |= xrays
+                // 7k/4r3/4q3/8/4Q3/3P1B2/8/K7 b - - 0 1
+                // the black rook was covered and opened up by black queen currently not considered
+
+                // println!("xray");
+                // game.visualize(may_xray & !attadef);
+                attadef |= game.board().pseudo_attacks_to(m.get_dest(), may_xray & !attadef, combined);
+            }
+
+            may_xray &= !from;
 
             stm = !stm;
             from = smallest_attacker(attadef, stm, game);
@@ -47,6 +59,7 @@ pub fn see(game: &Game, m: ChessMove) -> i16 {
 
             if from.0 == 0 { break };
         }
+        // println!("{max_d}");
 
         for inv_d in 1..max_d {
             let d = max_d - inv_d;
